b621792de41901a15118c6e56972a2e2b3356cd9
Vladimir Khleborodov <khleborodov@hotmail.com>
2023-12-25 18:41:57 +0300

    Задание 'Найти min/max в строке' выполнено

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' ');
+  let filteredArr = arr.filter(item => !isNaN(item));
+  let min = Math.min(...filteredArr);
+  let max = Math.max(...filteredArr);
+  let result = {
+    min,
+    max
+  };
+
+  return result;
 }
