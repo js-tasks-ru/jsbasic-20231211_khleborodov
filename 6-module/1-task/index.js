e54167d05aa94163dbbb6dc218ba409558f07dc3
Vladimir Khleborodov <khleborodov@hotmail.com>
2024-01-12 17:49:54 +0300

    Задание 'Таблица с удаляемыми строками' выполнено

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,39 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.table = document.createElement('table');
+    this.table.insertAdjacentHTML('beforeEnd', `<thead>
+                                                  <tr>
+                                                    <th>Имя</th>
+                                                    <th>Возраст</th>
+                                                    <th>Зарплата</th>
+                                                    <th>Город</th>
+                                                    <th></th>
+                                                  </tr>
+                                                </thead>
+                                                <tbody>
+                                                </tbody>`);
+    document.body.appendChild(this.table);
+    
+    this.elem = this.table;
+    
+    const tbody = this.elem.querySelector('tbody');
+    for (const {name, age, salary, city} of rows) {
+      tbody.insertAdjacentHTML('beforeEnd', ` <tr>
+                                                <td>${name}</td>
+                                                <td>${age}</td>
+                                                <td>${salary}</td>
+                                                <td>${city}</td>
+                                                <td><button>X</button>
+                                                </td>
+                                              </tr>`);
+    }
+    
+    tbody.addEventListener('click', (event) => {
+      if (event.target.matches('button')) {
+        let deletableElem = event.target.closest('tr');
+        deletableElem.remove();
+      }
+    });
   }
 }
